f3a346ec19baca98e186a30b8011a076ac8840af
Alejandro Recio <alejandrorecio.r@gmail.com>
2020-02-29 20:22:14 +0100

    .

--- conn/conn.php
+++ conn/conn.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "127.0.0.1:3307";
-   $username = "root";
-   $password = "";
-   $dbname = "gasolineras";
+$servername = "Localhost";
+$username = "arfix311_arfix31";
+$password = "Arronches123";
+$dbname = "arfix311_arfix";
 
-   $conn = new mysqli($servername, $username, $password, $dbname);
-
-   if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-   }
- ?>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
